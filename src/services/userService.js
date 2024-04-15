870f439a5b12bd40e09c4937a6a6128ad8e80727
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-04-15 22:14:20 +0700

    done login WB-5

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,6 @@ import axios from "../axios";
 let handleLoginService = (email, password) => {
   return axios.post("/api/user/login", { email: email, password: password });
 };
-
 let handleCreatCart = (data) => {
   return axios.post("/api/cart/create-cart", data);
 };
