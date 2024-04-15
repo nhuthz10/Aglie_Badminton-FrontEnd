9e449cc02bfefb70c048643cce2855ae973fce85
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-04-15 19:48:57 +0700

    call api, validate, done login page <WB-6>

--- src/redux-toolkit/userSlice.js
+++ src/redux-toolkit/userSlice.js
@@ -2,14 +2,38 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   login: false,
+  userInfo: [],
+  favourites: [],
+  cartId: null,
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    logIn: (state, action) => {
+      state.login = true;
+      state.userInfo = action.payload;
+    },
+    logOut: (state) => {
+      state.login = false;
+      state.userInfo = [];
+      state.favourites = [];
+      state.cartId = null;
+    },
+    updateAvatar: (state, action) => {
+      state.userInfo.avatar = action.payload;
+    },
+    updateFavourites: (state, action) => {
+      state.favourites = action.payload;
+    },
+    updateCartId: (state, action) => {
+      state.cartId = action.payload;
+    },
+  },
 });
 
-export const {} = userSlice.actions;
+export const { logOut, logIn, updateAvatar, updateFavourites, updateCartId } =
+  userSlice.actions;
 
 export default userSlice.reducer;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,11 @@
 import axios from "../axios";
+
+let handleLoginService = (email, password) => {
+  return axios.post("/api/user/login", { email: email, password: password });
+};
+
+let handleCreatCart = (data) => {
+  return axios.post("/api/cart/create-cart", data);
+};
+
+export { handleLoginService, handleCreatCart };
