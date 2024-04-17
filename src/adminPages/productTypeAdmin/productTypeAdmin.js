740236864d7fb26f0cc4e54997283663ada32f33
myngocvo <vtmn22070312@gmail.com>
2024-04-17 23:46:19 +0700

    FrontEnd View Product Category [WB-83]

--- src/adminPages/productTypeAdmin/productTypeAdmin.js
+++ src/adminPages/productTypeAdmin/productTypeAdmin.js
@@ -1,44 +1,8 @@
 import React, { useEffect } from "react";
-import { toast } from "react-toastify";
-import { handleDeleteProductTypeService } from "../../services/productService";
-import {
-  fetchAllProductTypeRedux,
-  loadingAdmin,
-} from "../../redux-toolkit/adminSlice";
-import { useDispatch, useSelector } from "react-redux";
 import "../admin.scss";
 import GridData from "../../components/gridData";
-import { LIMIT } from "../../utils";
 
 function ProductTypeAdmin() {
-  const dispatch = useDispatch();
-  const page = useSelector((state) => state.pagination.page);
-
-  const handleDeleteProductType = async (productType) => {
-    try {
-      dispatch(loadingAdmin(true));
-      let res = await handleDeleteProductTypeService(productType.id);
-      if (res && res.errCode === 0) {
-        dispatch(
-          fetchAllProductTypeRedux({
-            limit: LIMIT,
-            page: page,
-            pagination: true,
-          })
-        );
-        toast.success("Xóa loại sản phẩm thành công");
-      }
-    } catch (err) {
-      if (err?.response?.data?.errCode === 2) {
-        toast.error("Loại sản phẩm không tồn tại");
-      } else {
-        toast.error(err?.response?.data?.message);
-      }
-    } finally {
-      dispatch(loadingAdmin(false));
-    }
-  };
-
   const tableColumns = [
     {
       label: "STT",
@@ -53,7 +17,6 @@ function ProductTypeAdmin() {
   return (
     <GridData
       headerString="Quản lý loại sản phẩm"
-      handleDelete={handleDeleteProductType}
       tableColumns={tableColumns}
       gridType={"product-type"}
     />
