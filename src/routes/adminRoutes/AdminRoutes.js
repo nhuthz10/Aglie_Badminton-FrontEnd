9475184c2d5cf8cb083387dab65b089f66e0d7d6
ThanhHuyen1605 <huyenchu.160503@gmail.com>
2024-05-08 18:37:16 +0700

    fix

--- src/routes/adminRoutes/AdminRoutes.js
+++ src/routes/adminRoutes/AdminRoutes.js
@@ -2,25 +2,50 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Error from "../../pages/error/Error";
+import OverView from "../../system/orverView/OverView";
 import { path } from "../../utils";
+import UserAdmin from "../../system/userAdmin/userAdmin";
 import SizeAdmin from "../../system/sizeAdmin/sizeAdmin";
 import ProductTypeAdmin from "../../system/productTypeAdmin/productTypeAdmin";
 import BrandAdmin from "../../system/brandAdmin/brandAdmin";
 import ProductAdmin from "../../system/productAdmin/productAdmin";
+import UserPost from "../../system/userAdmin/UserPost";
 import ProductTypePost from "../../system/productTypeAdmin/ProductTypePost";
 import SizePost from "../../system/sizeAdmin/SizePost";
 import BrandPost from "../../system/brandAdmin/brandPost";
 import ProductPost from "../../system/productAdmin/ProductPost";
 import ProductSizeAdmin from "../../system/productAdmin/productSizeAdmin";
 import ProductSizePost from "../../system/productAdmin/ProductSizePost";
+import VoucherAdmin from "../../system/voucherAdmin/VoucherAdmin";
+import VoucherPost from "../../system/voucherAdmin/VoucherPost";
+import OrderAdmin from "../../system/orderAdmin/orderAdmin";
+import OrderPost from "../../system/orderAdmin/orderPost";
+import ReportAdmin from "../../system/reportAdmin/ReportAdmin";
 
 function AdminRoutes() {
   return (
     <Routes>
+      <Route path={path.OVERVIEW_ADMIN} element={<OverView />} />
       <Route path={path.PRODUCT_ADMIN} element={<ProductAdmin />} />
       <Route path={path.PRODUCT_TYPE_ADMIN} element={<ProductTypeAdmin />} />
       <Route path={path.PRODUCT_BRAND_ADMIN} element={<BrandAdmin />} />
       <Route path={path.PRODUCT_SIZE_ADMIN} element={<SizeAdmin />} />
+      <Route path={path.USER_ADMIN} element={<UserAdmin />} />
+      <Route path={path.VOUCHER_ADMIN} element={<VoucherAdmin />} />
+      <Route path={path.REVENUE_ADMIN} element={<ReportAdmin />} />
+      <Route path={path.ORDER_WAITING_ADMIN} element={<OrderAdmin />} />
+      <Route path={path.ORDER_DELIVERY_ADMIN} element={<OrderAdmin />} />
+      <Route path={path.ORDER_DONE_ADMIN} element={<OrderAdmin />} />
+      <Route path={path.ORDER_CANCELED_ADMIN} element={<OrderAdmin />} />
+      <Route path={path.ORDER_DETAIL} element={<OrderPost />} />
+      <Route
+        path={`${path.USER_ADMIN}/${path.POST_ADMIN}`}
+        element={<UserPost />}
+      />
+      <Route
+        path={`${path.USER_ADMIN}/${path.PUT_ADMIN}`}
+        element={<UserPost />}
+      />
       <Route
         path={`${path.PRODUCT_TYPE_ADMIN}/${path.POST_ADMIN}`}
         element={<ProductTypePost />}
@@ -64,6 +89,14 @@ function AdminRoutes() {
       <Route
         path={`${path.PRODUCT_ADMIN}/${path.PRODUCT_PRODUCTSIZE_ADMIN}/${path.PUT_ADMIN}`}
         element={<ProductSizePost />}
+      />
+      <Route
+        path={`${path.VOUCHER_ADMIN}/${path.POST_ADMIN}`}
+        element={<VoucherPost />}
+      />
+      <Route
+        path={`${path.VOUCHER_ADMIN}/${path.PUT_ADMIN}`}
+        element={<VoucherPost />}
       />
       <Route path="*" element={<Error />} />
     </Routes>
